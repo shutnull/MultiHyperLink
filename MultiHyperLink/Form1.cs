f6f64efc47adc51d564273cc398ed52d43aa5c97
shutnull <49683848+shutnull@users.noreply.github.com>
2021-08-27 01:21:20 +0900

    プロジェクト ファイルを追加します。

---
+++ MultiHyperLink/Form1.cs
@@ -0,0 +1,39 @@
+﻿using MultiHyperLink.UserControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+
+namespace MultiHyperLink
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Dictionary<string, string> textToLink = new Dictionary<string, string>()
+            {
+                { "テスト", @"C:\Users\shutnull\Pictures\ホロライブ" },
+                { "テストおおおおおおおおおおおおお", @"C:\Users\shutnull\Pictures" },
+                { "llllllllllllllllllllllllllllllllllllllllllllllllllllll", @"C:\Users\shutnull" },
+                { "A", @"C:\Users" },
+            };
+
+            HyperLinkMultiLabel hyperLinkMultiLabel = new HyperLinkMultiLabel(textToLink);
+
+            ElementHost elementHost = new ElementHost();
+            elementHost.Child = hyperLinkMultiLabel;
+            this.Controls.Add(elementHost);
+        }
+    }
+}
---
+++ MultiHyperLink/UserControls/HyperLinkMultiLabel.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace MultiHyperLink.UserControls
+{
+    /// <summary>
+    /// HyperLinkMultiLabel.xaml の相互作用ロジック
+    /// </summary>
+    public partial class HyperLinkMultiLabel : UserControl
+    {
+        public HyperLinkMultiLabel()
+        {
+
+        }
+
+        public HyperLinkMultiLabel(Dictionary<string, string> TextToLink)
+        {
+            InitializeComponent();
+
+            foreach (string text in TextToLink.Keys)
+            {
+                Label label = new Label()
+                {
+                    Background = Brushes.Transparent,
+                    HorizontalContentAlignment = HorizontalAlignment.Center,
+                    VerticalContentAlignment = VerticalAlignment.Center
+                };
+                Hyperlink link = new Hyperlink();
+                link.Inlines.Add(text);
+                link.NavigateUri = new Uri(TextToLink[text]);
+                link.RequestNavigate += Link_RequestNavigate;
+                label.Content = link;
+                LabelPanel.Children.Add(label);
+            }
+        }
+
+        private void Link_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.LocalPath));
+            e.Handled = true;
+        }
+    }
+}
